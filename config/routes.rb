49960430f9184fb2edf0a8b08b422caabcc9afa1
Ashwin Saval <ashwinsaval@gmail.com>
2014-04-26 15:11:05 +0530

    * BUGFIX

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Yuva::Application.routes.draw do
   root :to => 'issues#index'  
   devise_for :users
 
-  get "/issues", :to => "issues#index", :as => issue_list
-  get "/issues/:id", :to => "issues#issue", :as => issue_details
+  get "/issues", :to => "issues#index", :as => "issue_list"
+  get "/issues/:id", :to => "issues#issue", :as => "issue_details"
   post "/issues/:id/resolve", :to => "issues#resolve"
   post "/issues/:id/process", :to => "issues#process"
 
